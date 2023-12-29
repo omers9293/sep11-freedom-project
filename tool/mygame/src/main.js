709a6e4a82f440e5314d24973c3fef4364759341
Omer Saleh <omers7@nycstudents.net>
2023-12-29 00:08:24 -0500

    12/28 tinkering

---
+++ tool/mygame/src/main.js
@@ -0,0 +1,103 @@
+import kaboom from "kaboom";
+
+// Initialize context
+kaboom();
+
+// Load assets
+loadSprite("birdy", "sprites/birdy.png");
+loadSprite("bg", "sprites/bg.png");
+loadSprite("pipe", "sprites/pipe.png");
+
+let highScore = 0;
+
+scene("game", () => {
+  const PIPE_GAP = 120;
+  let score = 0;
+
+  add([
+    sprite("bg", {width: width(), height: height()})
+  ]);
+
+  const scoreText = add([
+    text(score, {size: 50})
+  ]);
+
+  // Add a game object to the screen
+  const player = add([
+    // List of components
+    sprite("birdy"),
+    scale(2),
+    pos(80, 40),
+    area(),
+    body(),
+  ]);
+
+  function producePipes(){
+    const offset = rand(-50, 50);
+
+    add([
+      sprite("pipe"),
+      pos(width(), height()/2 + offset + PIPE_GAP/2),
+      "pipe",
+      area(),
+      {passed: false}
+    ]);
+
+    add([
+      sprite("pipe", {flipY: true}),
+      pos(width(), height()/2 + offset - PIPE_GAP/2),
+      origin("botleft"),
+      "pipe",
+      area()
+    ]);
+  }
+
+  loop(1.5, () => {
+    producePipes();
+  });
+
+  action("pipe", (pipe) => {
+    pipe.move(-160, 0);
+
+    if (pipe.passed === false && pipe.pos.x < player.pos.x) {
+      pipe.passed = true;
+      score += 1;
+      scoreText.text = score;
+    }
+  });
+
+  player.collides("pipe", () => {
+    go("gameover", score);
+  });
+
+  player.action(() => {
+    if (player.pos.y > height() + 30 || player.pos.y < -30) {
+      go("gameover", score);
+    }
+  });
+
+  keyPress("space", () => {
+    player.jump(400);
+  });
+});
+
+scene("gameover", (score) => {
+  if (score > highScore) {
+    highScore = score;
+  }
+
+  add([
+    text(
+      "Game Over!\n"
+      + "Score: " + score
+      + "\nHigh Score: " + highScore,
+      {size: 45}
+    )
+  ]);
+
+  keyPress("space", () => {
+    go("game");
+  });
+});
+
+go("game");
